6246ea056514d2ea977c828f5a1a5723c17867f1
Pedro Henrique Fernandino <110419385+PedroHenrique061@users.noreply.github.com>
2024-06-14 11:26:24 -0300

    Update TF_2C2_Controle_Final_por_Dupla_PedroSilva.sql

--- Trabalho Final/TF_2C2_Controle_Final_por_Dupla_PedroSilva.sql
+++ Trabalho Final/TF_2C2_Controle_Final_por_Dupla_PedroSilva.sql
@@ -1,1 +1,14 @@
-
+-- ---------------------   << EVENTOS  >>   ---------------------
+--
+--                    SCRIPT DE CRIAÇÃO DE USUÁRIOS (DDL)
+-- 
+-- Data Criacao ...........: 14/06/2024
+-- Autor(es) ..............: Pedro H. Silva e Sabrina C. Berno
+-- Banco de Dados .........: MySQL
+-- Banco de Dados(nome) ...: TF2
+-- 
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 25 Tabelas
+-- 
+-- -----------------------------------------------------------------
