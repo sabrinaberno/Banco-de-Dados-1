cae87b1288b9f5aaadeb030a65146ed8738406fd
Sabrina Berno <sabrinaberno@hotmail.com>
2024-06-16 15:14:52 -0300

    Criando perfis de usuário, usuários e concessão de papéis

--- Trabalho Final/TF_2C2_Controle_Final_por_Dupla_PedroSilva.sql
+++ Trabalho Final/TF_2C2_Controle_Final_por_Dupla_PedroSilva.sql
@@ -1,6 +1,7 @@
+
 -- ---------------------   << EVENTOS  >>   ---------------------
 --
---                    SCRIPT DE CRIAÇÃO DE USUÁRIOS (DDL)
+--                    SCRIPT DE CONTROLE (DDL)
 -- 
 -- Data Criacao ...........: 14/06/2024
 -- Autor(es) ..............: Pedro H. Silva e Sabrina C. Berno
@@ -13,8 +14,28 @@
 -- 
 -- -----------------------------------------------------------------
 
-CREATE USER 'gerente'@'localhost' IDENTIFIED BY 'ger2024';
-GRANT ALL PRIVILEGES ON  TF2.* TO 'gerente'@'localhost';
+USE TF2;
 
-CREATE USER 'funcionario'@'localhost' IDENTIFIED BY 'fun2024';
-GRANT SELECT ON  TF2.* TO 'funcionario'@'localhost';
+-- Criando perfis de usuário
+CREATE ROLE 'admin';
+CREATE ROLE 'funcionario';
+
+-- Concessão de privilégios aos papéis
+GRANT ALL PRIVILEGES ON TF2.* TO 'admin';
+GRANT SELECT ON TF2.* TO 'funcionario';
+
+-- Criação de usuários E Concessão de papéis
+CREATE USER 'sabrina'@'localhost' IDENTIFIED BY 'SAb@2024#';
+GRANT 'admin' TO 'sabrina'@'localhost';
+
+CREATE USER 'Jonathan'@'localhost' IDENTIFIED BY 'JOn@2024#';
+GRANT 'admin' TO 'sabrina'@'localhost';
+
+CREATE USER 'luis'@'localhost' IDENTIFIED BY 'Lui@2024!';
+GRANT 'funcionario' TO 'luis'@'localhost';
+
+CREATE USER 'natalia'@'localhost' IDENTIFIED BY 'Nat@2024!';
+GRANT 'funcionario' TO 'natalia'@'localhost';
+
+
+
